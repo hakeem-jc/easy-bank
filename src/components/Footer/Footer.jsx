87cd197a262e64532a416400e178a2a2233ab5c3
hakeem-jc <hclarke.dev@gmail.com>
2022-12-21 11:43:22 -0500

    Create footer component as placeholder

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="Footer">
+            About Us
+            Contact
+            Blog
+            Careers
+            Support
+            Privacy Policy
+
+            Request Invite
+
+            © Easybank. All Rights Reserved
+            
+            <div class="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/hakeem-jc" rel="noreferrer" target="_blank">Your Name Here</a>.
+            </div>
+        </footer>
+    );
+}
